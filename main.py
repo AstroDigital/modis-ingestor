e227df59b8cb6fb131934e28a3e3ed8d8b67c44a
Drew Bollinger <drew@developmentseed.org>
2017-01-10 16:05:46 -0500

    Unhardcode main script

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ from lib.core import query_cmr, download, convert_to_geotiff, push_to_s3, get_s3
 def main(date):
     tiles = query_cmr(date, date)
     for tile in tiles:
-        file = 'MCD43A4.A2011358.h03v11.006.2016139165208.hdf'
-        # file = download(tile['url'])
+        file = download(tile['url'])
         files = convert_to_geotiff(file)
         folder = get_s3_folder(file)
         index_fname = make_index(tile['thumb'], file, files)
-        files = [index_fname]
+        files.append(index_fname)
         for f in files:
             push_to_s3(f, 'modis-pds', folder)
 
