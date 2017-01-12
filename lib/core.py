036970f1a3c27b63e1fce75904ac9104c1a9bdf6
Matthew Hanson <matt.a.hanson@gmail.com>
2017-01-12 15:43:15 -0500

    fix bandname, band types, workaround for gain

--- lib/core.py
+++ lib/core.py
@@ -133,11 +133,15 @@ def convert_to_geotiff(hdf, path=''):
         file_names.append(metadata_fname)
     # save each band as a TIF
     for i, band in enumerate(img):
-        fname = hdf.replace('.hdf', '') + '_B' + str(i).zfill(2) + '.TIF'
+        gain = band.gain()
+        offset = band.offset()
+        band.set_gain(1.0)
+        band.set_offset(0.0)
+        fname = hdf.replace('.hdf', '') + '_B' + str(i+1).zfill(2) + '.TIF'
         print('Writing %s' % fname)
-        a = gippy.GeoImage.create_from(img, fname, nb=1)
-        a.add_band(img[i])
-        a.save(fname)
+        imgout = img.select([i+1]).save(fname)
+        imgout.set_gain(gain)
+        imgout.set_offset(offset)
         file_names.append(fname)
 
     return file_names
