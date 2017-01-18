df2f0feb7782d5020dc028dc32496164a7cb69c1
Matthew Hanson <matt.a.hanson@gmail.com>
2017-01-17 20:38:19 -0500

    use pycmr

--- modispds/cmr.py
+++ modispds/cmr.py
@@ -5,9 +5,11 @@ Client library for using NASAs CMR API for searching and downloading from NASA d
 import os
 import requests
 import sys
-from urllib2 import HTTPError, urlopen
-from json import load
-from codecs import getreader
+import datetime
+from dateutil.parser import parse
+from urllib2 import HTTPError
+from json import dump
+import logging
 from requests.packages.urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
 if sys.version_info < (3, 0):
@@ -15,71 +17,105 @@ if sys.version_info < (3, 0):
 else:
     from html.parser import HTMLParser
 
+# get environment variables
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
-
-PROVIDER = os.getenv('PROVIDER', 'LPDAAC_ECS')
-PRODUCT = os.getenv('PRODUCT', 'MCD43A4.006')
-PAGE_SIZE = int(os.getenv('PAGE_SIZE', '1000'))
-
 EARTHDATA_USER = os.getenv('EARTHDATA_USER')
 EARTHDATA_PASS = os.getenv('EARTHDATA_PASS')
 
+# pyCMR
+from pyCMR import CMR
+cmr = CMR(os.path.join(os.path.dirname(__file__), 'cmr.cfg'))
 
-def query(start_date, end_date):
+# product specific settings
+products = {
+    'MCD43A4': {
+        'day_offset': 8
+    }
+}
+
+# logging
+log = logging.getLogger('modispds')
+
+
+def query(start_date, end_date, product='MCD43A4.006', provider='LPDAAC_ECS'):
     """ Search CMR database for spectral MODIS tiles matching a temporal range,
     defined by a start date and end date. Returns metadata containing the URL of
     each image.
     """
+    prod, ver = product.split('.')
+    temporal = '{0}T00:00:00Z,{1}T23:59:00Z'.format(start_date, end_date)
+    start_date = parse(start_date)
+    end_date = parse(end_date)
 
-    prod, ver = PRODUCT.split('.')
-    query = 'https://cmr.earthdata.nasa.gov/search/granules.json?' + \
-            'provider={0}&short_name={1}&version={2}'.format(
-             PROVIDER, prod, ver) + \
-            '&online_only=true&page_size={0}&sort_key=start_date'.format(
-             PAGE_SIZE) + \
-            '&temporal={0}T00:00:00Z,{1}T23:59:00Z'.format(
-             start_date, end_date)
-    print('searching ' + query)
     try:
-        response = urlopen(query)
+        _granules = cmr.searchGranule(provider=provider, short_name=prod, version=ver,
+                                      temporal=temporal, sort_key='start_date', limit=10000)
     except HTTPError as error:
-        raise ValueError('The CMR server returned the following error:',
-                         error.read())
+        raise ValueError('Error with CMR:', error.read())
 
-    json_data = load(getreader("utf-8")(response))
-    tile_meta = []
-    for entry in json_data['feed']['entry']:
-        date = entry['time_start'].split('T')[0]
-        entry_meta = {'date': date}
-        for url_entry in entry['links']:
-            if url_entry.get('type') == 'application/x-hdfeos' and 'opendap' not in url_entry.get('href'):
-                entry_meta['url'] = str(url_entry['href'])
-            if url_entry.get('type') == 'image/jpeg':
-                entry_meta['thumb'] = url_entry['href']
-
-        tile_meta.append(entry_meta)
-    response.close()
-
-    return tile_meta
+    # filter dates
+    granules = []
+    for gran in _granules:
+        dt = gran['Granule']['Temporal']['RangeDateTime']['BeginningDateTime'].split('T')[0]
+        date = parse(dt) + datetime.timedelta(days=products[prod]['day_offset'])
+        if (start_date <= date <= end_date):
+            granules.append(gran)
+    return granules
 
 
-def set_retries(retries):
+def download_granule(meta, outdir=''):
+    """ Download granule files from metadata instance """
+    # get basename
+    url = meta['Granule']['OnlineAccessURLs']['OnlineAccessURL']['URL']
+    bname = os.path.splitext(os.path.basename(url))[0]
+
+    # save metadata
+    fn_meta = os.path.join(outdir, bname + '_meta.json')
+    with open(fn_meta, 'w') as f:
+        log.info('Writing metadata to %s' % fn_meta)
+        dump(meta, f, sort_keys=True, indent=4, ensure_ascii=False)
+
+    # download hdf
+    fn_hdf = download_file(url, outdir=outdir)
+
+    links = {r['Type']: r['URL'] for r in meta['Granule']['OnlineResources']['OnlineResource']}
+    # download xml metadata
+    fn_metaxml = download_file(links['METADATA'], outdir=outdir)
+    # download browse image
+    fn_browse = download_file(links['BROWSE'], noauth=True, outdir=outdir)
+
+    return [fn_hdf, fn_browse, fn_meta, fn_metaxml]
+
+
+def download_file(url, noauth=False, outdir=''):
+    """ Get URL and save with some name """
+    fout = os.path.join(outdir, os.path.basename(url))
+    auth = () if noauth else (EARTHDATA_USER, EARTHDATA_PASS)
+    # download as stream
+    session = get_session(retries=5)
+    if noauth:
+        stream = session.get(url, stream=True)
+    else:
+        stream = get_stream(session, url, auth, [])
+    chunk_size = 1024
+    try:
+        with open(fout, 'wb') as f:
+            log.info('Saving %s' % fout)
+            for chunk in stream.iter_content(chunk_size):
+                f.write(chunk)
+    except:
+        raise Exception("Problem fetching %s" % stream)
+
+    return fout
+
+
+def get_session(retries=5):
     s = requests.Session()
     r = Retry(total=retries, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
     s.mount('http://', HTTPAdapter(max_retries=r))
     s.mount('https://', HTTPAdapter(max_retries=r))
     return s
-
-
-class LinkFinder(HTMLParser):
-    def __init__(self):
-        self.reset()
-        self.download_link = None
-
-    def handle_starttag(self, tag, attrs):
-        if attrs and attrs[0][0] == 'href':
-            self.download_link = attrs[0][1]
 
 
 def get_stream(session, url, auth, previous_tries):
@@ -97,20 +133,11 @@ def get_stream(session, url, auth, previous_tries):
         raise Exception("Earthdata Authentication Error")
 
 
-def download(url, path=''):
-    """ Get URL and save with some name """
-    fout = os.path.join(path, os.path.basename(url))
-    auth = (EARTHDATA_USER, EARTHDATA_PASS)
-    # download as stream
-    session = set_retries(5)
-    stream = get_stream(session, url, auth, [])
-    chunk_size = 1024
-    try:
-        with open(fout, 'wb') as f:
-            print('Writing %s' % fout)
-            for chunk in stream.iter_content(chunk_size):
-                f.write(chunk)
-    except:
-        raise Exception("Problem downloading %s" % stream)
+class LinkFinder(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.download_link = None
 
-    return fout
+    def handle_starttag(self, tag, attrs):
+        if attrs and attrs[0][0] == 'href':
+            self.download_link = attrs[0][1]
