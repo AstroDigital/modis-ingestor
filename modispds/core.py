29f6ea84c86c349d1a8d0a57b2b5a3ac25dd40dc
Matthew Hanson <matt.a.hanson@gmail.com>
2017-01-24 14:08:16 -0500

    added gdal create options

--- modispds/core.py
+++ modispds/core.py
@@ -2,9 +2,11 @@ import sys
 import time
 import os
 import logging
+import argparse
 from modispds.cmr import query, download_granule
 from modispds.pds import push_to_s3, make_index
 import gippy
+from modispds.version import __version__
 
 logger = logging.getLogger(__name__)
 
@@ -30,18 +32,19 @@ def convert_to_geotiff(hdf, outdir=''):
     bname = os.path.basename(hdf)
     file_names = []
     img = gippy.GeoImage(hdf, True)
+    opts = {'COMPRESS': 'DEFLATE', 'PREDICTOR': '2', 'TILED': 'YES', 'BLOCKXSIZE': '512', 'BLOCKYSIZE': '512'}
     # save each band as a TIF
     for i, band in enumerate(img):
         fname = os.path.join(outdir, bname.replace('.hdf', '') + '_B' + str(i+1).zfill(2) + '.TIF')
         logger.info('Writing %s' % fname)
-        imgout = img.select([i+1]).save(fname)
+        imgout = img.select([i+1]).save(fname, overviews=True, options=opts)
         file_names.append(fname)
 
     return file_names
 
 
-def main(date, outdir=''):
-    granules = query(date, date)
+def main(date1, date2, outdir=''):
+    granules = query(date1, date2)
 
     for gran in granules[0:1]:
         url = gran['Granule']['OnlineAccessURLs']['OnlineAccessURL']['URL']
@@ -69,5 +72,23 @@ def main(date, outdir=''):
         logger.info('Completed processing granule %s in : %ss' % (bname, time.time() - start_time))
 
 
+def parse_args(args):
+    """ Parse arguments for the NDWI algorithm """
+    desc = 'MODIS Public Dataset Utility (v%s)' % __version__
+    dhf = argparse.ArgumentDefaultsHelpFormatter
+    parser = argparse.ArgumentParser(description=desc, formatter_class=dhf)
+    parser.add_argument('--version', help='Print version and exit', action='version', version=__version__)
+
+    parser.add_argument('start_date', help='First date')
+    parser.add_argument('end_date', help='End date')
+
+    return parser.parse_args(args)
+
+
 def cli():
-    main(date=sys.argv[1])
+    args = parse_args(sys.argv[1:])
+    main(args.start_date, args.end_date)
+
+
+if __name__ == "__main__":
+    cli()
