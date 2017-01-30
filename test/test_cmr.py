f6c6409ab05e9ce7e5164e752f1a5e7ab8f8142c
Matthew Hanson <matt.a.hanson@gmail.com>
2017-01-30 15:40:06 -0500

    expand tests, increase limit for dealing with date ranges

--- test/test_cmr.py
+++ test/test_cmr.py
@@ -7,6 +7,7 @@ class TestCMR(unittest.TestCase):
     """ Test query and downloading from CMR """
 
     date1 = '2016-01-01'
+    date2 = '2016-01-02'
     url = 'http://e4ftl01.cr.usgs.gov//MODV6_Cmp_B/MOTA/MCD43A4.006/2016.01.01/MCD43A4.A2016001.h11v12.006.2016174075640.hdf'
 
     @classmethod
@@ -19,6 +20,21 @@ class TestCMR(unittest.TestCase):
         self.assertEqual(len(self.q), 299)
         keys = self.q[0].keys()
         self.assertTrue('Granule' in keys)
+
+    def test_query_2days(self):
+        """ Query CMR for two days """
+        q = query(self.date1, self.date2)
+        self.assertEqual(len(q), 598)
+
+    def _test_query_5days(self):
+        """ Query CMR for five days """
+        q = query(self.date1, '2016-01-05')
+        self.assertEqual(len(q), 1497)
+
+    def _test_query_30days(self):
+        """ Query CMR for 30 days """
+        q = query('2016-01-01', '2016-01-30')
+        self.assertEqual(len(q), 9272)
 
     def test_download(self):
         """ Download a file from CMR """
