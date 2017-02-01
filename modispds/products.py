4ea9663862bbb5d0904372b5f53e38862668d5c6
Matthew Hanson <matt.a.hanson@gmail.com>
2017-02-01 17:23:08 -0500

    product configuration, set proper bandnames + tests, fixes #27

---
+++ modispds/products.py
@@ -0,0 +1,10 @@
+# MODIS product configuration
+
+products = {
+    'MCD43A4.006': {
+        'day_offset': 8,
+        'bandnames':
+            ['B%sqa' % str(i).zfill(2) for i in range(1, 8)] + \
+            ['B%s' % str(i).zfill(2) for i in range(1, 8)]
+    }
+}
