bb1742d797ec1f556cf5309dae82ccbeec1590e1
Matthew Hanson <matt.a.hanson@gmail.com>
2017-03-22 15:16:37 -0400

    start adding other products

--- modispds/products.py
+++ modispds/products.py
@@ -7,5 +7,25 @@ products = {
             ['B%sqa' % str(i).zfill(2) for i in range(1, 8)] +
             ['B%s' % str(i).zfill(2) for i in range(1, 8)],
         'overviews': ([False] * 7) + ([True] * 7)
+    },
+    'MOD09GA.006': {
+        'day_offset': 0,
+        'bandnames': [],
+        'overviews': []
+    },
+    'MYD09GA.006': {
+        'day_offset': 0,
+        'bandnames': [],
+        'overviews': []
+    },
+    'MOD09GQ.006': {
+        'day_offset': 0,
+        'bandnames': [],
+        'overviews': []
+    },
+    'MYD09GQ.006': {
+        'day_offset': 0,
+        'bandnames': [],
+        'overviews': []
     }
 }
